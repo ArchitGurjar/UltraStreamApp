3e709b9120efd844c1d140f982317ddc433f4d72
Archit Gurjar <koshindersingh169@gmail.com>
2026-07-23 21:07:12 +0530

    Align MainActivity with traditional ViewBinding architecture and remove conflicting Compose screen

---
+++ app/src/main/java/com/ultrastream/MainActivity.kt
@@ -0,0 +1,35 @@
+// app/src/main/java/com/ultrastream/MainActivity.kt
+package com.ultrastream
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ultrastream.databinding.ActivityMainBinding
+import com.ultrastream.ui.details.DetailsActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
+    }
+
+    companion object {
+        const val REQUEST_CODE_PLAYER = 1001
+    }
+}
